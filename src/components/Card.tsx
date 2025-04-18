e72fa042853c151afe98366784dbee44b89f6047
BaeJuyoung <83051136+Baeju0@users.noreply.github.com>
2025-04-18 18:15:26 +0900

    Card 컴포넌트의 스타일 수정 및 title prop 추가

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,26 @@
 import {HTMLAttributes, ReactNode} from "react";
 
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
+    title: string;
     children: ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({
-                                       children,
-                                       className='',
-                                       ...rest
-    }) => (
+export function Card ({
+    title,
+    children,
+    className='',
+    ...rest
+    }: CardProps) {
+    return (
         <div
             {...rest}
             className={`card ${className}`}
     >
-        {children}
-    </div>
-)
+            <h3 className={`title-text ${className}`}>{title}</h3>
+            {children}
+        </div>
+    );
+}
 
 
 export default Card;
