514c5613c2a44695fd8e9dad2d877bb83afdcea1
BaeJuyoung <83051136+Baeju0@users.noreply.github.com>
2025-04-18 18:51:07 +0900

    페이지 레이아웃 설정 & 시멘틱 태그 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import './index.css';
-import Button from "./components/Button.tsx";
+import Card from "./components/Card.tsx";
 
 function App() {
-    const onClickButton = (): void => {
-        console.log("clicked!");
-    }
 
     return (
-        <div className="min-h-screen bg-gradient">
-            <h1 className="text-4xl font-bold text-blue-500">
-                Hihihihi
-            </h1>
-            <Button
-                onClick={onClickButton}>
-                Click Button!
-            </Button>
+        <div className="w-full min-h-screen bg-gradient">
+
+            <div className="p-20">
+            <header>
+                <h1 className="text-2xl font-bold mb-6 text-center">따뜻한 하루를 시작해요!</h1>
+            </header>
+
+
+            <main className="grid grid-cols-2 grid-rows-min gap-6 h-full max-w-4xl mx-auto px-4 py-10">
+                <Card title="추천 활동" className="col-start-1 row-start-1">
+                    컴포넌트 추가
+                </Card>
+
+                <Card title="달력"
+                      className="col-start-2 row-start-1 row-span-2">
+                    컴포넌트 추가
+                </Card>
+
+                <Card title="데이터" className="col-start-1 row-start-2">
+                    컴포넌트 추가
+                </Card>
+            </main>
+            </div>
+
+            <footer className="bg-black text-white p-6 text-center text-sm">
+                &copy; Baeju0
+            </footer>
         </div>
     );
 }
