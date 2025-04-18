155ac5a9cb99047a7ab796272afec75c0143b80a
BaeJuyoung <83051136+Baeju0@users.noreply.github.com>
2025-04-18 21:15:33 +0900

    기본 레이아웃 수정 및 lucide-react 라이브러리 설치 후 아이콘 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,53 @@
 import './index.css';
 import Card from "./components/Card.tsx";
+import Button from "./components/Button.tsx";
+import {Heart, Menu} from "lucide-react";
 
 function App() {
 
     return (
         <div className="w-full min-h-screen bg-gradient">
 
-            <div className="p-20">
-            <header>
-                <h1 className="text-2xl font-bold mb-6 text-center">따뜻한 하루를 시작해요!</h1>
-            </header>
+            <div className="p-10">
+                <header className="flex items-center justify-between px-20 py-5">
+                    <h1>여기 로고</h1>
+                    <div className="flex items-center space-x-4">
+                        <button
+                            aria-label="메뉴 열기"
+                            onClick={() => {
+                                // onClick 로직 작성
+                            }}
+                        >
+                            <Menu size={28} color="#934311"/>
+                        </button>
+                    </div>
+                </header>
 
+                <section className="grid grid-cols-3 items-center max-w-4xl mx-auto px-4 py-5">
+                    <div />
+                    <h1 className="base-text">
+                        따뜻한 하루를 시작해요!
+                    </h1>
+                    <div className="col-start-3 justify-self-end">
+                        <Button>
+                            <Heart color="#FFFFFF" className="mr-2"/>오늘의 기분</Button>
+                    </div>
+                </section>
 
-            <main className="grid grid-cols-2 grid-rows-min gap-6 h-full max-w-4xl mx-auto px-4 py-10">
-                <Card title="추천 활동" className="col-start-1 row-start-1">
-                    컴포넌트 추가
-                </Card>
+                <main className="grid grid-cols-2 grid-rows-min gap-6 h-full max-w-4xl mx-auto px-4 py-10">
+                    <Card title="추천 활동" className="col-start-1 row-start-1">
+                        컴포넌트 추가 공간
+                    </Card>
 
-                <Card title="달력"
-                      className="col-start-2 row-start-1 row-span-2">
-                    컴포넌트 추가
-                </Card>
+                    <Card title="달력"
+                          className="col-start-2 row-start-1 row-span-2">
+                        컴포넌트 추가 공간
+                    </Card>
 
-                <Card title="데이터" className="col-start-1 row-start-2">
-                    컴포넌트 추가
-                </Card>
-            </main>
+                    <Card title="데이터" className="col-start-1 row-start-2">
+                        컴포넌트 추가 공간
+                    </Card>
+                </main>
             </div>
 
             <footer className="bg-black text-white p-6 text-center text-sm">
