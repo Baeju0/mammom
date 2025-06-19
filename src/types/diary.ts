d70d40e24b84e513148e256c92159bd64e635466
BaeJuyoung <83051136+Baeju0@users.noreply.github.com>
2025-06-19 20:53:59 +0900

    - diary 타입 중 emotion_color, symptom, custom_emotion_color, custom_symptom 에 null 타입도 지정

--- src/types/diary.ts
+++ src/types/diary.ts
@@ -2,10 +2,10 @@ export interface DiaryEntryInsert {
     title: string;
     content: string;
     entry_date: string;
-    emotion_color_id?: number;
-    symptom_id?: number;
-    custom_emotion_color?: string;
-    custom_symptom?: string;
+    emotion_color_id?: number | null;
+    symptom_id?: number | null;
+    custom_emotion_color?: string | null;
+    custom_symptom?: string | null;
     profile_id: string;
 }
 
@@ -14,10 +14,10 @@ export interface DiaryEntry {
     title: string;
     content: string;
     entry_date: string;
-    emotion_color_id?: number;
-    symptom_id?: number;
-    custom_emotion_color?: string;
-    custom_symptom?: string;
+    emotion_color_id?: number | null;
+    symptom_id?: number | null;
+    custom_emotion_color?: string | null;
+    custom_symptom?: string | null;
     profile_id: string;
     created_at?: string;
     updated_at?: string;
