3270670db25207bf54c6d0e3cc0146e7d3eb17ee
BaeJuyoung <83051136+Baeju0@users.noreply.github.com>
2025-06-25 15:37:37 +0900

    - 감정 색상, 신체 증상이 DB에 있는 기본 값인지, 커스텀인지 찾는 로직 개선

--- src/util/getEmotionSymptomEntry.ts
+++ src/util/getEmotionSymptomEntry.ts
@@ -12,19 +12,21 @@ export function getEmotionSymptomEntry(
     emotionColors: EmotionColor[],
     symptoms: Symptom[],
 ): EmotionSymptomEntry {
+    // 감정 색상이 기본값인지 커스텀인지 찾기
     const emotionRecord = emotionColors.find((color) => color.id === entry.emotion_color_id!);
-    const emotion = entry.custom_emotion_color
-        ? { name : "기타", hex_code : entry.custom_emotion_color}
-        : emotionRecord
-        ? { name: emotionRecord.name ?? "", hex_code: emotionRecord.hex_code}
-        : { name: "알 수 없음", hex_code: "#000000"};
+    const emotion = entry.custom_emotion_color && entry.custom_emotion_color.trim() !== ""
+                    ? {name: "기타", hex_code: entry.custom_emotion_color}
+                    : emotionRecord
+                    ? {name: emotionRecord.name || "", hex_code: emotionRecord.hex_code}
+                    : {name: "알 수 없음", hex_code: "#000000"};
 
+    // 신체 증상이 기본값인지 커스텀인지 찾기
     const symptomRecord = symptoms.find((symptom) => symptom.id === entry.symptom_id!);
-    const symptom = entry.custom_symptom
-        ? { name: entry.custom_symptom, emoji: "🤔" }
-        : symptomRecord
-        ? { name: symptomRecord.name, emoji: symptomRecord.emoji}
-        : { name: "알 수 없음", emoji: "🫥" };
+    const symptom = entry.custom_symptom && entry.custom_symptom.trim() !== ""
+                    ? {name: entry.custom_symptom, emoji: "🤔"}
+                    : symptomRecord
+                    ? {name: symptomRecord.name, emoji: symptomRecord.emoji}
+                    : {name: "알 수 없음", emoji: "🫥"}
 
     return {emotion, symptom};
 }
